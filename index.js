1c593f08c52823246382a33170c6ffc1042add2c
Soul Codes <reach.soul.codes@gmail.com>
2018-11-29 09:21:04 +0100

    prevent not matching anything

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const concurrently = require("concurrently");
 const colors = ["green", "yellow", "blue", "magenta", "cyan"];
 
 const workspaces = JSON.parse(execSync("yarn workspaces info"));
+const regexText = process.argv[2] || ".*";
+console.log(
+  `Launching watch scripts for workspace packages matching regex /${regexText}/`
+);
+
 const regex = new RegExp(process.argv[2] || ".*");
 const targets = Object.keys(workspaces).filter(package => regex.test(package));
+if (!targets) {
+  console.error("No workspace package matched. Not doing anything.");
+  process.exit(1);
+}
 
 console.log(
   `Running watch scripts for:\n${targets
